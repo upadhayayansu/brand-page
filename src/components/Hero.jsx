0e0d542e7a06451422ff01180fd933612ce70726
upadhayayansu <upadhayayansu8@gmail.com>
2023-09-26 20:52:03 +0545

    route issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,13 @@ const HeroSection = () => {
       <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST </h1>
         <p>
-          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES
           YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES
         </p>
       </div>
       <div className="hero-btn">
         <button>Shop Now</button>
+        <div className="secondary-button"></div>
         <button>Category</button>
       </div>
 
@@ -17,6 +18,7 @@ const HeroSection = () => {
         <p>Also Available On</p>
       </div>
 
+      <div className="hero-content"></div>
       <div className="hero-image"></div>
     </main>
   );
