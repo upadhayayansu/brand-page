5a5f76bd198b0881580af08c1a59742859d84e68
upadhayayansu <upadhayayansu8@gmail.com>
2023-09-26 18:49:20 +0545

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const HeroSection = () => {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST </h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES
+        </p>
+      </div>
+      <div className="hero-btn">
+        <button>Shop Now</button>
+        <button>Category</button>
+      </div>
+
+      <div className="shoppping">
+        <p>Also Available On</p>
+      </div>
+
+      <div className="hero-image"></div>
+    </main>
+  );
+};
+export default HeroSection;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
