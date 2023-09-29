d1248157c43f2ac5c46fb8bd97dbf9186fe68038
upadhayayansu <upadhayayansu8@gmail.com>
2023-09-29 12:28:51 +0545

    route issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,13 @@ const HeroSection = () => {
       <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST </h1>
         <p>
-          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES
-          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
         </p>
+      </div>
+      <div className="hero-image">
+        <img src="/images/shoe_image.png" alt="shoe" />
       </div>
       <div className="hero-btn">
         <button>Shop Now</button>
@@ -16,6 +20,11 @@ const HeroSection = () => {
 
       <div className="shoppping">
         <p>Also Available On</p>
+      </div>
+
+      <div className="brand-icons">
+        <img src="/images/flipkart.png" alt="flipkart-logo" />
+        <img src="/images/amazon.png" alt="amazon-logo" />
       </div>
 
       <div className="hero-content"></div>
